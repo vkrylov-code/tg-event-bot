67b998c1bf8c4ce82906151b85a41d15c7baafbf
Viktor Krylov <v.krylov84@gmail.com>
2025-08-29 15:10:18 +0300

    Переход на webhook

--- tg_event_bot.py
+++ tg_event_bot.py
@@ -263,7 +263,7 @@ if __name__ == "__main__":
     # Flask сам по себе блокирующий, запускаем с threaded=True
     app.run(
         host="0.0.0.0",
-        port=443,
+        port=8443,
         ssl_context=(f"{SSL_PATH}/fullchain.pem", f"{SSL_PATH}/privkey.pem"),
         threaded=True
     )
